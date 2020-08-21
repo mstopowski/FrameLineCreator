61e0eb1e4539e4308ebc5b851d66434f822915ba
mstopowski <49496093+mstopowski@users.noreply.github.com>
2020-08-21 14:28:02 +0200

    Start of the project
    First commit

---
+++ FrameLineCreator/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional
+// These will show in Rhino's option dialog, in the tab Plug-ins
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+
+// Rhino requires a Guid assigned to the assembly.
+[assembly: Guid("4E7434B5-F2B8-4366-AC13-3095312E3A67")]
