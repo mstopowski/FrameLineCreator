ae75d70ed631bd99f11465c641bbdeac76a6c02d
mstopowski <49496093+mstopowski@users.noreply.github.com>
2020-09-03 21:14:34 +0200

    First working version

--- FrameLineCreator/AssemblyInfo.cs
+++ FrameLineCreator/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using Rhino.PlugIns;
 // These will show in Rhino's option dialog, in the tab Plug-ins
 [assembly: PlugInDescription(DescriptionType.Address, "")]
 [assembly: PlugInDescription(DescriptionType.Country, "")]
-[assembly: PlugInDescription(DescriptionType.Email, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "maurycy.stopowski@gmail.com")]
 [assembly: PlugInDescription(DescriptionType.Phone, "")]
 [assembly: PlugInDescription(DescriptionType.Organization, "")]
 [assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
--- FrameLineCreator/FrameLineCreatorCommand.cs
+++ FrameLineCreator/FrameLineCreatorCommand.cs
@@ -1,6 +1,8 @@
 using System;
 using Rhino;
 using Rhino.Commands;
+using Rhino.Collections;
+using Rhino.Display;
 using Rhino.Geometry;
 using Rhino.Input;
 using Rhino.Input.Custom;
@@ -12,45 +14,166 @@ namespace FrameLineCreator
 {
     public class FrameLineCreatorCommand : Rhino.Commands.Command
     {
-        public override string EnglishName => "FrameLineCreatorCommand";
+        public override string EnglishName => "CreateFrameLine";
 
         protected override Result RunCommand(Rhino.RhinoDoc doc, RunMode mode)
         {
-            // TODO: start here modifying the behaviour of your command.
-            // ---
-            RhinoApp.WriteLine("The {0} command will add a line right now.", EnglishName);
+            // Data from user
+            int startFrame = 0;
+            int endFrame = 0;
+            int spacing = 0;
+            bool modify = false;
 
-            Point3d pt0;
-            using (GetPoint getPointAction = new GetPoint())
+            // Variables to capture user input
+            int startMod = 0;
+            int endMod = 0;
+            int spacMod = 0;
+
+            // List to capture modifications in frameline spacing
+            RhinoList<int> startModList = new RhinoList<int>();
+            RhinoList<int> endModList = new RhinoList<int>();
+            RhinoList<int> spacModList = new RhinoList<int>();
+
+            // Lists: spacing, frame numbers, if frame to be labeled
+            RhinoList<int> spacingList = new RhinoList<int>();
+            RhinoList<int> framesList = new RhinoList<int>();
+            RhinoList<bool> ifLabelList = new RhinoList<bool>();
+
+            // Move distance in x-axis for zero to be at zero
+            int zeroMove = 0;
+
+            // Points for main line of frameline
+            RhinoList<Point3d> polyPoints = new RhinoList<Point3d>();
+
+            int tempSum = 0;
+
+            // Vertical lines height
+            int frameHeight = 400;
+
+            RhinoGet.GetInteger("Enter starting frame number: ", false, ref startFrame);
+            RhinoGet.GetInteger("Enter ending frame number: ", false, ref endFrame);
+            RhinoGet.GetInteger("Enter spacing: ", false, ref spacing);
+            RhinoGet.GetBool("Do you want to insert local modification of spacing?", true, "No", "Yes", ref modify);
+
+            while (modify)
             {
-                getPointAction.SetCommandPrompt("Please select the start point");
-                if (getPointAction.Get() != GetResult.Point)
-                {
-                    RhinoApp.WriteLine("No start point was selected.");
-                    return getPointAction.CommandResult();
-                }
-                pt0 = getPointAction.Point();
+                RhinoGet.GetInteger("Enter starting frame of modification: ", false, ref startMod);
+                RhinoGet.GetInteger("Enter ending frame of modification: ", false, ref endMod);
+                RhinoGet.GetInteger("Enter spacing of modification: ", false, ref spacMod);
+
+                startModList.Add(startMod);
+                endModList.Add(endMod);
+                spacModList.Add(spacMod);
+
+                RhinoGet.GetBool("Do you want to add another local modification?", true, "No", "Yes", ref modify);
             }
 
-            Point3d pt1;
-            using (GetPoint getPointAction = new GetPoint())
+            for (int i = 0; i < (endFrame - startFrame) + 1; i++)
             {
-                getPointAction.SetCommandPrompt("Please select the end point");
-                getPointAction.SetBasePoint(pt0, true);
-                getPointAction.DrawLineFromPoint(pt0, true);
-                if (getPointAction.Get() != GetResult.Point)
+                spacingList.Add(spacing);
+                framesList.Add(startFrame + i);
+                if ((startFrame + i) % 5 == 0)
                 {
-                    RhinoApp.WriteLine("No end point was selected.");
-                    return getPointAction.CommandResult();
+                    ifLabelList.Add(true);
                 }
-                pt1 = getPointAction.Point();
+                else
+                {
+                    ifLabelList.Add(false);
+                }
             }
 
-            doc.Objects.AddLine(pt0, pt1);
-            doc.Views.Redraw();
-            RhinoApp.WriteLine("The {0} command added one line to the document.", EnglishName);
+            if (startModList.Count > 0)
+            {
+                for (int i = 0; i < startModList.Count; i++)
+                {
+                    for (int j = 0; j < (endModList[i] - startModList[i]); j++)
+                    {
+                        spacingList[startModList[i] - startFrame + j] = spacModList[i];
+                    }
+                    ifLabelList[startModList[i] - startFrame] = true;
+                    ifLabelList[endModList[i] - startFrame] = true;
+                }
+            }
 
+            // First and last frame always with label
+            ifLabelList[0] = true;
+            ifLabelList[ifLabelList.Count - 1] = true;
 
+            for (int i = 0; i < framesList.Count; i++)
+            {
+                if (framesList[i] < 0)
+                {
+                    zeroMove += spacingList[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+                        
+            polyPoints.Add(new Point3d(-zeroMove, 0, 0));
+
+            for (int i = 0; i < framesList.Count; i++)
+            {
+                polyPoints.Add(new Point3d(spacingList[i] + tempSum - zeroMove, 0.0, 0.0));
+                tempSum += spacingList[i];
+            }
+                        
+            // Removing last point (end+1)
+            polyPoints.RemoveAt(polyPoints.Count - 1);
+
+            // Backup of current layer
+            Rhino.DocObjects.Layer layerBackUp = RhinoDoc.ActiveDoc.Layers.CurrentLayer;
+            int indexDef = layerBackUp.Index;
+
+            //Creating layer for frameline
+            Rhino.DocObjects.Layer flineLayer = new Rhino.DocObjects.Layer
+            {
+                Name = "Frameline"
+            };
+            int indexFL = doc.Layers.Add(flineLayer);         
+            RhinoDoc.ActiveDoc.Layers.SetCurrentLayerIndex(indexFL, true);
+
+            int textHeight = 150;
+
+            for (int i = 0; i < polyPoints.Count; i++)
+            {
+                doc.Objects.AddLine(new Line(new Point3d(polyPoints[i][0], polyPoints[i][1] - frameHeight / 2, 0),
+                                             new Point3d(polyPoints[i][0], polyPoints[i][1] + frameHeight / 2, 0)));
+                doc.Objects.AddLine(new Line(new Point3d(polyPoints[i][0], 0, polyPoints[i][1] - frameHeight / 2),
+                                             new Point3d(polyPoints[i][0], 0, polyPoints[i][1] + frameHeight / 2)));
+                if (ifLabelList[i])
+                {
+                    Text3d tkst = new Text3d("Fr " + framesList[i].ToString());
+                    Text3d tkstRotated = new Text3d("Fr " + framesList[i].ToString());
+
+                    tkst.Height = textHeight;
+                    tkstRotated.Height = textHeight;
+
+                    tkst.HorizontalAlignment = Rhino.DocObjects.TextHorizontalAlignment.Center;
+                    tkst.VerticalAlignment = Rhino.DocObjects.TextVerticalAlignment.Middle;
+
+                    tkstRotated.HorizontalAlignment = Rhino.DocObjects.TextHorizontalAlignment.Center;
+                    tkstRotated.VerticalAlignment = Rhino.DocObjects.TextVerticalAlignment.Middle;
+
+                    tkst.TextPlane = new Plane(new Point3d(polyPoints[i][0], - frameHeight, 0), new Vector3d(0.0, 0.0, 1.0));
+                    Plane rotPlane = new Plane(new Point3d(polyPoints[i][0], 0, -frameHeight), new Vector3d(0.0, 0.0, 1.0));
+                    rotPlane.Rotate(Math.PI / 2, new Vector3d(1.0, 0.0, 0.0));
+                    tkstRotated.TextPlane = rotPlane;
+                    
+                    doc.Objects.AddText(tkst);
+                    doc.Objects.AddText(tkstRotated); 
+                }
+            }
+
+            // Adding polyline to document
+            doc.Objects.AddPolyline(polyPoints);
+            RhinoDoc.ActiveDoc.Views.Redraw();
+
+            // Backing up to previous layer and locking frameline layer
+            RhinoDoc.ActiveDoc.Layers.SetCurrentLayerIndex(indexDef, true);
+            RhinoDoc.ActiveDoc.Layers.FindIndex(indexFL).IsLocked = true;
+            
             return Result.Success;
         }
     }
